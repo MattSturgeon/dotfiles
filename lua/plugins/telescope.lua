67481dd575f2a0adae5e4e8e69334ad08a61b255
Matt Sturgeon <matt@sturgeon.me.uk>
2023-05-20 23:24:23 +0100

    Move telescope keybinds to lazy table
    Enables lazy loading and simplifies config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,28 @@ return {
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    cmd = 'Telescope',
+    keys = {
+      {'<leader>?', function() require('telescope.builtin').oldfiles() end, desc = '[?] Find recently opened files'},
+      {'<leader><space>', function() require('telescope.builtin').buffers() end, desc = '[ ] Find existing buffers' },
+      {
+        '<leader>/',
+        function()
+          -- You can pass additional configuration to telescope to change theme, layout, etc.
+          require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+            winblend = 10,
+            previewer = false,
+          })
+        end,
+        desc = '[/] Fuzzily search in current buffer',
+      },
+      {'<leader>gf', function() require('telescope.builtin').git_files() end, desc = 'Search [G]it [F]iles' },
+      {'<leader>sf', function() require('telescope.builtin').find_files() end, desc = '[S]earch [F]iles' },
+      {'<leader>sh', function() require('telescope.builtin').help_tags() end, desc = '[S]earch [H]elp' },
+      {'<leader>sw', function() require('telescope.builtin').grep_string() end, desc = '[S]earch current [W]ord' },
+      {'<leader>sg', function() require('telescope.builtin').live_grep() end, desc = '[S]earch by [G]rep' },
+      {'<leader>sd', function() require('telescope.builtin').diagnostics() end, desc = '[S]earch [D]iagnostics' },
+    },
     config = function()
       -- See `:help telescope` and `:help telescope.setup()`
       require('telescope').setup {
