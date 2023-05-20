a9ac81733927958e6454a945bf755a17dfb238a7
Matt Sturgeon <matt@sturgeon.me.uk>
2023-05-20 02:19:30 +0100

    Initial structure +lazy.nvim

---
+++ lua/config/lazy.lua
@@ -0,0 +1,10 @@
+-- bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.runtimepath:prepend(lazypath)
+
+-- Install everything in lua/plugins
+require("lazy").setup("plugins")
---
+++ lua/config/options.lua
@@ -0,0 +1,6 @@
+local o = vim.opt
+
+o.scrolloff = 8
+o.sidescrolloff = 8
+
+o.smartindent = true
