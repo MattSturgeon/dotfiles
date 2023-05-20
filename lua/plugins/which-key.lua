5a8df8512ff769a427cc50fd5919282a90803a56
Matt Sturgeon <matt@sturgeon.me.uk>
2023-05-20 23:45:25 +0100

    Add WhichKey

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,19 @@
+return {
+  "folke/which-key.nvim",
+  config = function()
+    vim.o.timeout = true
+    vim.o.timeoutlen = 300
+    local wk = require("which-key")
+    wk.setup({
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    })
+    wk.register({
+      -- Register some groups
+      f = { name = 'file' },
+      g = { name = 'git' },
+      s = { name = 'search' },
+    }, { prefix = '<leader>' })
+  end,
+}
